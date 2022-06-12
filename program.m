7025fd9d6cb3fcfb61fde4978a2b050c8753642b
João Pedro Augusto Costa <jpac1207@gmail.com>
2022-06-12 02:01:34 -0300

    Add clausules to save parameters of training

--- program.m
+++ program.m
@@ -2,24 +2,30 @@
 neuralNetworks = getNeuralNetworks();
 doTraining(neuralNetworks)
 
+% Realiza o treinamento das redes neurais utilizando os dados já
+% processados, além de salvar os pesos aprendidos.
 function doTraining(neuralNetworks)
     processed_dataset = load('processed_dataset.mat');
-    X = processed_dataset.X;
+    X = processed_dataset.X;    
     Y = processed_dataset.Y;
     X_norm = normalizeInput(X);
+    neuralNetworksCount = size(neuralNetworks, 2);
     [X_train, Y_train, X_val, Y_val, X_test, Y_test] = splitData(X_norm, Y);
+    inputSize  = size(X, 2);
 
-    % Compute the total number of epochs
-    
+    % Computa número total de epócas    
     totalEpochs = 0;            
-    for i=1:size(neuralNetworks, 2)        
+    for i=1:neuralNetworksCount        
         totalEpochs = totalEpochs + neuralNetworks(i).numberOfEpochs;
     end
     allNetworksErrors = zeros(totalEpochs, 1);
     allNetworksValErrors = zeros(totalEpochs, 1);
     start = 1;
+    
+    % Salva informações básicas
+    save("./ann_weigths/basic_info", "neuralNetworksCount", "inputSize");
 
-    for i=1:size(neuralNetworks, 2)        
+    for i=1:neuralNetworksCount        
         [hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, ...
             outputVsHiddenBias, finalErrors, finalValErrors, ...
             trainingFinalPredictions, validationFinalPredictions] = trainMLP(neuralNetworks(i).I, ...
@@ -27,10 +33,12 @@ function doTraining(neuralNetworks)
             neuralNetworks(i).eta, 0, X_train', Y_train, X_val', Y_val, neuralNetworks(i).isTheFirstNetwork);     
         allNetworksErrors(start:(start+neuralNetworks(i).numberOfEpochs-1), 1) = finalErrors;
         allNetworksValErrors(start:(start+neuralNetworks(i).numberOfEpochs-1), 1) = finalValErrors;
-        Y_train = (Y_train .* (max(Y_train)-trainingFinalPredictions))
+        Y_train = (Y_train .* (max(Y_train)-trainingFinalPredictions));
         Y_val = (Y_val .* (max(Y_val)-validationFinalPredictions));        
-        start = start + neuralNetworks(i).numberOfEpochs;
+        start = start + neuralNetworks(i).numberOfEpochs;       
         %trainingFinalPredictions
+        % Saving the weights
+        save("./ann_weigths/ann_weights_" + i + ".mat", "hiddenVsInputWeights", "hiddenVsInputBias", "outputVsHiddenWeights", "outputVsHiddenBias");
     end  
     plot(1:totalEpochs, allNetworksErrors, 'o');
     hold on;
@@ -39,7 +47,7 @@ function doTraining(neuralNetworks)
     legend('Média Erros Treinamento MLP', 'Média Erros Validação MLP');
     ylabel('Erro Quadrático Médio');
     xlabel('Épocas');
-    title('Erros de Treino e Validação do Treinamento');  
+    title('Erros de Treino e Validação do Treinamento');      
 end
 
 % Realiza o preenchimento do array 'neuralNetworks' com os parâmetros de
